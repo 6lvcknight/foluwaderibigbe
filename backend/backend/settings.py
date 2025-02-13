35e6cb7cc1dcc6c479c1b735641e601570f80211
Folu Aderibgbe <127684059+6lvcknight@users.noreply.github.com>
2025-02-13 15:46:31 -0500

    chore(hosts): adjust hosts for communication

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env('BASE_URL')]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
