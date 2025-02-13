2aefcccda78486aea65702bf7f1e6dfc6b43d5cb
Folu Aderibgbe <127684059+6lvcknight@users.noreply.github.com>
2025-02-13 14:25:40 -0500

    refactor(navbar)

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,29 +1,38 @@
 import { Menu, X } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import logo from "../assets/ia-high-resolution-logo-white-transparent.png";
-import { navItems } from "../constants";
 import { NavLink } from 'react-router-dom';
+import checkAuth from "../store/auth";
 
 
 const Navbar = () => {
-  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const toggleNavbar = () => {
-    setMobileDrawerOpen(!mobileDrawerOpen);
-  };
+  useEffect(() => {
+    setIsLoggedIn(checkAuth());
+  }, []);
 
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg mb-20">
       <div className="container px-4 mx-auto relative lg:text-sm">
         <div className="flex justify-between items-center">
-          <NavLink to="/foluwaderibigbe">
+          <NavLink to="/">
             <div className="flex items-center flex-shrink-0">
               <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
               <span className="text-xl tracking-tight">Folu Aderibigbe</span>
             </div>
           </NavLink>
-          <div className="lg:hidden md:flex flex-col justify-end">
-          </div>
+
+          {isLoggedIn && (
+            <div className="flex gap-4 justify-end ml-2">
+              <a href="/dashboard" className="hover:underline cursor-pointer">
+                Dashboard
+              </a>
+              <a href="/logout" className="hover:underline cursor-pointer">
+                Sign out
+              </a>
+            </div>
+          )}
         </div>
       </div>
     </nav>
