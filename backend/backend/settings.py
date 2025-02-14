8f9a9997cca3dcd6cf6d5c8e843789f0d7df25fb
Folu Aderibgbe <127684059+6lvcknight@users.noreply.github.com>
2025-02-13 20:51:34 -0500

    ci(backend)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = [env('BASE_URL')]
+ALLOWED_HOSTS = [env('BASE_URL'), '127.0.0.1', 'localhost']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
