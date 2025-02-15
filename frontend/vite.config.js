57e1366f87a0ca0d6b33c9cc63fec4961f86c4b0
Folu Aderibgbe <127684059+6lvcknight@users.noreply.github.com>
2025-02-15 07:26:44 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base: "/",
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://backend.railway.internal:8080',
+        changeOrigin: true,
+        secure: false,
+      },
+    }
 })
