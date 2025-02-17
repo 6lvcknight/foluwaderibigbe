b1c3c53a7483fe75ac93df86a06e74a7ea0279bf
Folu Aderibgbe <127684059+6lvcknight@users.noreply.github.com>
2025-02-17 18:20:10 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,19 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://backend-production-4c8b.up.railway.app/',
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  }, 
-  build: {
-    sourcemap: false,  // Disable source maps in production to prevent errors
-  },
-  define: {
-    'process.env': {},  // Fix issues related to process.env in Vite
-  },
 });
